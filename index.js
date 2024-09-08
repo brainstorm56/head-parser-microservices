c77e390c7b716c9066894b098e3be94d5a34ad4d
Abhinav Gupta <abhinav6483@gmail.com>
2024-09-09 02:37:29 +0530

    implemented the functionality

--- index.js
+++ index.js
@@ -18,7 +18,13 @@ app.use(express.static('public'));
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
-
+app.get('/api/whoami',(req,res)=>{
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  })
+})
 // your first API endpoint...
 app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
